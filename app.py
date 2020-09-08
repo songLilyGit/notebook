b0107a64043c4c5126e73bb6a38e8257bdef5be0
songLilyGit <3383191557@qq.com>
2020-09-08 10:25:47 +0800

    数据库关系创建

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import click
 import os
 app = Flask(__name__) #定义flask的实例
 
-##再试试
+
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret string')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost:3306/test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
@@ -23,10 +23,161 @@ class Note(db.Model):
     def __repr__(self):
         return '<Note %r>' % self.body
 
+# one to many
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    phone = db.Column(db.String(20))
+    articles = db.relationship('Article')  # collection
+
+    def __repr__(self):
+        return '<Author %r>' % self.name
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), index=True)
+    body = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+
+    def __repr__(self):
+        return '<Article %r>' % self.title
+
+# many to one
+class Citizen(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    city_id = db.Column(db.Integer, db.ForeignKey('city.id'))
+    city = db.relationship('City')  # scalar
+
+    def __repr__(self):
+        return '<Citizen %r>' % self.name
+
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+
+    def __repr__(self):
+        return '<City %r>' % self.name
+
+# one to one
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    capital = db.relationship('Capital', uselist=False)  # collection -> scalar
+
+    def __repr__(self):
+        return '<Country %r>' % self.name
+
+
+class Capital(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
+    country = db.relationship('Country')  # scalar
+
+    def __repr__(self):
+        return '<Capital %r>' % self.name
+# many to many with association table
+association_table = db.Table('association',
+                             db.Column('student_id', db.Integer, db.ForeignKey('student.id')),
+                             db.Column('teacher_id', db.Integer, db.ForeignKey('teacher.id'))
+                             )
+
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    grade = db.Column(db.String(20))
+    teachers = db.relationship('Teacher',
+                               secondary=association_table,
+                               back_populates='students')  # collection
+
+    def __repr__(self):
+        return '<Student %r>' % self.name
+
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    office = db.Column(db.String(20))
+    students = db.relationship('Student',
+                               secondary=association_table,
+                               back_populates='teachers')  # collection
+
+    def __repr__(self):
+        return '<Teacher %r>' % self.name
+
+
+# one to many + bidirectional relationship
+class Writer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    books = db.relationship('Book', back_populates='writer')
+
+    def __repr__(self):
+        return '<Writer %r>' % self.name
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True)
+    writer_id = db.Column(db.Integer, db.ForeignKey('writer.id'))
+    writer = db.relationship('Writer', back_populates='books')
+
+    def __repr__(self):
+        return '<Book %r>' % self.name
+
+# one to many + bidirectional relationship + use backref to declare bidirectional relationship
+class Singer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    songs = db.relationship('Song', backref='singer')
+
+    def __repr__(self):
+        return '<Singer %r>' % self.name
+
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True)
+    singer_id = db.Column(db.Integer, db.ForeignKey('singer.id'))
+
+    def __repr__(self):
+        return '<Song %r>' % self.name
+
+# cascade
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.Text)
+    comments = db.relationship('Comment', back_populates='post', cascade='all, delete-orphan')  # collection
+
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    post = db.relationship('Post', back_populates='comments')  # scalar
+
+
+
 @app.shell_context_processor
 def sehll_context():
-    return {'db':db,'Note':Note}
-#
+    return dict(db=db,Note=Note,Author=Author,Article=Article)
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
 @app.cli.command()
 def init_db():
     db.create_all()
